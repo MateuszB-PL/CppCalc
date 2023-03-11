4114e5c374ad35884f2ddcffaced49793b0185a3
MatioXPL <matiox.endercore@gmail.com>
2023-03-11 15:10:05 +0100

    installation script- ./install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#installation script - DO NOT EDIT
 sudo echo "installing in //etc" && sudo mkdir //etc/cppcalc && sudo cp -a install/resources //etc/cppcalc/ && cd ~ && echo "alias cppcalc='cd / && cd /etc/cppcalc/resources/ && ./calc'" >> .bashrc
 
 
