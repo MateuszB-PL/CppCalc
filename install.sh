510ce16502c1efdd6bfe1308203e89f1f3681f60
MatioXPL <99821157+MatioXPL@users.noreply.github.com>
2023-04-01 17:02:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #installation script - DO NOT EDIT
-sudo echo "installing in //etc" && sudo mkdir //etc/cppcalc && sudo cp -a install/resources //etc/cppcalc/ && cd ~ && echo "alias cppcalc='cd / && cd /etc/cppcalc/resources/ && ./calc'" >> .bashrc
-
-
+if [ "$EUID" -ne 0 ]
+  then sudo echo "installing in //etc" && sudo mkdir //etc/cppcalc && sudo cp -a install/resources //etc/cppcalc/ && cd ~ && echo "alias cppcalc='cd / && cd /etc/cppcalc/resources/ && ./calc'" >> .bashrc
+  exit
+else
+  echo "run without root"
+  exit
+fi
